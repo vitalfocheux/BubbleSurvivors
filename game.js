3bb0f7fa59de38b4029c23bc7a9f1151048f47c0
vitalfocheux <vital.focheux@edu.univ-fcomte.fr>
2025-01-25 11:53:23 +0100

    structuration pour importer des modules

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+import { Enemy } from './Enemy.js';
+
 // Game configuration
 const CANVAS_WIDTH = 800;
 const CANVAS_HEIGHT = 600;
@@ -248,50 +250,6 @@ class Projectile {
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
         ctx.fillStyle = 'red';
         ctx.fill();
-    }
-}
-
-// class Enemy {
-//     constructor(canvas) {
-//         this.canvas = canvas;
-//         this.radius = 15;
-//         this.speed = 2;
-//         this.x = Math.random() * canvas.width;
-//         this.y = Math.random() * canvas.height;
-//     }
-
-//     update(playerX, playerY) {
-//         const angle = Math.atan2(playerY - this.y, playerX - this.x);
-//         this.x += Math.cos(angle) * this.speed;
-//         this.y += Math.sin(angle) * this.speed;
-//     }
-
-//     render(ctx) {
-//         ctx.beginPath();
-//         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
-//         ctx.fillStyle = 'green';
-//         ctx.fill();
-//     }
-// }
-
-class Enemy{
-    constructor(canvas, radius, speed) {
-        if(new.target === Enemy){
-            throw new TypeError("Cannot construct Enemy instances directly");
-        }
-        this.canvas = canvas;
-        this.radius = radius;
-        this.speed = speed;
-        this.x = Math.random() * canvas.width;
-        this.y = Math.random() * canvas.height;
-    }
-
-    update(playerX, playerY) {
-        throw new Error("Method 'update()' must be implemented.");
-    }
-
-    render(ctx) {
-        throw new Error("Method 'render()' must be implemented.");
     }
 }
 
