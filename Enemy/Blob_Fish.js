ffa33e9c9a6488df06fbb29aaca38d668ba1b33e
vitalfocheux <vital.focheux@edu.univ-fcomte.fr>
2025-01-25 15:13:18 +0100

    Fonction getExp

--- Enemy/Blob_Fish.js
+++ Enemy/Blob_Fish.js
@@ -12,6 +12,7 @@ export class Blob_Fish extends Enemy {
         this.frameHeight = 64; // Height of each frame
         this.frameCounter = 0; // Counter to control frame rate
         this.framesPerSprite = 10; // Number of game frames per sprite frame
+        this.exp = 1;
     }
 
     loadBlobFishSprite() {
@@ -71,5 +72,9 @@ export class Blob_Fish extends Enemy {
                 ctx.drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight);
             };
         }
+    }
+
+    getExp() {
+        return this.exp;
     }
 }
