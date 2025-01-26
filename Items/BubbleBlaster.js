ee0c4fdaac0cf480f26850e803800fa1789b5cea
vitalfocheux <vital.focheux@edu.univ-fcomte.fr>
2025-01-26 14:23:09 +0100

    Add BubbleBlaster

---
+++ Items/BubbleBlaster.js
@@ -0,0 +1,11 @@
+import { Item } from './Item.js';
+
+export class BubbleBlaster extends Item {
+    constructor() {
+        super(0, 0, 50, 0, 10);
+    }
+
+    getDescriptor() {
+        return `Augmente vos dégâts de ${super.getIncreaseDamage()}%`;
+    }
+}
